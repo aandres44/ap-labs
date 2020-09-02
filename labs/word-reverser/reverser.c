3f05776b3aff123488edde5d8448d564b375eb0a
Andres AA <obed.n.munoz@tecchuy27arts@gmail.com>
2020-09-02 01:13:04 +0000

    Solve reverser.c
    Signed-off-by: Andres AA <obed.n.munoz@tecchuy27arts@gmail.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+
+#define IN   1   /* inside a word */
+#define OUT  0   /* outside a word */
+
+
 int main(){
     // Place your magic here
+
+    //string
+    int i = 0,
+        j = 0;
+    char c[20];
+    while ((c[i] = getchar()) != EOF)
+    {
+        i++;
+        j++;
+    }
+    
+    i = j - 1;
+
+    while (i >= 0)
+    {
+        printf("%c", c[i]);
+        i--;
+    }
+    
+
     return 0;
 }
