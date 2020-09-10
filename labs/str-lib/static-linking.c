3a4e41e11ea3cf44d15a8c7fb221aaf7e62092b7
Andres AA <obed.n.munoz@tecchuy27arts@gmail.com>
2020-09-10 04:42:13 +0000

    str-lib v2
    Signed-off-by: Andres AA <obed.n.munoz@tecchuy27arts@gmail.com>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int mystrlen(char *);
+char *mystradd(char *, char *);
+int mystrfind(char *, char *);
+
+int main(int argc, char *argv[]) {
+
+    if(argc > 3) {
+        if(strcmp(argv[1], "-add") == 0) {
+            int len = mystrlen(argv[2]);
+            char* str = mystradd(argv[2], argv[3]);
+            int newLen = mystrlen(str);
+            printf("Initial Length      : %i\n", len);
+            printf("New String          : %s\n", str);
+            printf("New Length          : %i\n", newLen);
+        } else if(strcmp(argv[1], "-find") == 0){
+            int pos = mystrfind(argv[2], argv[3]);
+            printf("['%s'] sring was found at [%i] position\n", argv[3], pos);
+        } else{
+            printf("Invalid argument\n");
+        }
+    }
+
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
 
 
-int mystrlen(char *str){
-    return 0;
+
+int mystrlen(char *str) {
+
+    int size = 0;
+    while(str[size] != '\0') {
+        size++;
+    }
+    return size;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+
+
+char *mystradd(char *origin, char *addition) {
+
+    int len = mystrlen(origin);
+    int i;
+    for(i = 0; addition[i] != '\0'; i++){
+        origin[len + i] = addition[i];
+    }
+    origin[len + i] = '\0';
+    return origin;
+
 }
 
-int mystrfind(char *origin, char *substr){
-    return 0;
+int mystrfind(char *origin, char *substr) {
+
+    int len = mystrlen(origin);
+    int sublen = mystrlen(substr);
+    int i, j;
+
+    for(i = 0; i < len; i++){
+
+        if(origin[i] == substr[0]){
+
+            for (j = 1; j < sublen; j++){
+
+                if (origin[i + j] != substr[j]){
+                    break;
+                }
+            }
+            if(j == sublen){
+                return i;
+            }
+        }
+    }
+
+    return -1;
+
+    
 }
