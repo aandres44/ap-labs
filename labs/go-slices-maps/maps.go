77268c2a4461570c1d5de9b1aae13c83062a5480
Andres AA <obed.n.munoz@tecchuy27arts@gmail.com>
2020-09-23 04:29:55 +0000

    Solve go-maps
    Signed-off-by: Andres AA <obed.n.munoz@tecchuy27arts@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Split(s," ")
+	countMap := make(map[string]int)
+	for i:= 0; i < len(words); i++ {
+		countMap[words[i]] += 1
+	}
+	return countMap;
+}
+
 func main() {
+	wc.Test(WordCount)
 }
