8b51359543327f37dbee290680c92933ba2b4c15
Andres AA <obed.n.munoz@tecchuy27arts@gmail.com>
2020-09-23 04:19:50 +0000

    Solve go-slices
    Signed-off-by: Andres AA <obed.n.munoz@tecchuy27arts@gmail.com>

--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -2,8 +2,16 @@ package main
 
 import "golang.org/x/tour/pic"
 
+// Pic function from Golang tour.
 func Pic(dx, dy int) [][]uint8 {
-     return [][]uint8{}
+	img := make([][]uint8, dy)
+	for x := range img {
+		img[x] = make([]uint8, dx)
+		for y := 0; y < dx; y++ {
+			img[x][y] = uint8(y * x + x * y * 98 + 85)
+		}
+	}
+	return img
 }
 
 func main() {
